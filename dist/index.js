038e1cd8f96342bbfdde1c6c5715b3f86bdfdd9c
Ke1992 <740757242@qq.com>
2019-10-27 15:43:30 +0800

    feat: 增加基础命令

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
-console.error('hello world');
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const program = require("commander");
+// 配置
+const { version, } = require('../package.json'); // eslint-disable-line
+// 注册版本号
+program
+    .version(version, '-v, --version');
+// 没有输入任何命令，则输出help
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
+// 注册工具
+program.parse(process.argv);
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.error('hello world');
+#!/usr/bin/env node
+import * as program from 'commander';
+// 配置
+const {
+    version, // 当前工具版本号
+} = require('../package.json'); // eslint-disable-line
+
+// 注册版本号
+program
+    .version(version, '-v, --version');
+
+// 没有输入任何命令，则输出help
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
+
+// 注册工具
+program.parse(process.argv);
